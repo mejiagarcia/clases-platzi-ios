5bfb279d9f7bdbc85e5451958f3699429dede42d
Carlos Mejia <carlosmejia083@gmail.com>
2020-01-20 14:50:49 -0500

    Initial Commit

---
+++ Aprendiendo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aprendiendo
+//
+//  Created by Luis Carlos Mejia Garcia on 20/01/20.
+//  Copyright © 2020 Platzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
