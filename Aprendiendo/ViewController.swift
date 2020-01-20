852fa3f1d743d357a0ac0732d6333d95560b3edb
Carlos Mejia <carlosmejia083@gmail.com>
2020-01-20 16:15:08 -0500

    Clase 5 - Controles por código

--- Aprendiendo/ViewController.swift
+++ Aprendiendo/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Referencias UI
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBAction func loginButtonAction() {
+        // 1. Obtener los valores de los textfield.
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if email == "carlos@mejia.com", password == "123" {
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        } else {
+            print("Credenciales Inválidas.")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
